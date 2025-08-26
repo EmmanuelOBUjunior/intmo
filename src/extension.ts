6b72ada28c0c5b5447123498380353a57e460dc0
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 15:24:34 +0000

    feat:initializing spotify api with stored tokens

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,21 @@ import * as vscode from 'vscode';
 import SpotifyWebApi from 'spotify-web-api-node';
 
 
-let SpotifyApi = SpotifyWebApi;
+let spotifyApi: SpotifyWebApi;
 
 export function activate(context: vscode.ExtensionContext) {
+	//Initialize Spotify API with stored tokens
+	spotifyApi = new SpotifyWebApi({
+		clientId: context.globalState.get('clientId') as string || '',
+		clientSecret: context.globalState.get('clientSecret') as string || '',
+		redirectUri: context.globalState.get('redirectUri') as string || ''
+	});
 
-
-	console.log('Congratulations, your extension "intmo" is now active!');
-
+	console.log("Spotify API initialized", spotifyApi);
+	const getApi = vscode.commands.registerCommand('intmo.getApi', () => {
+		vscode.window.showInformationMessage('Getting Spotify API instance...');
+		return spotifyApi;
+	});
 	
 	const disposable = vscode.commands.registerCommand('intmo.helloWorld', () => {
 		
@@ -16,6 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(getApi);
 }
 
 
