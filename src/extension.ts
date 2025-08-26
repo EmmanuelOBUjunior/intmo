df2e700dda36604fae2883afa3d9d6232eb30b7c
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 19:03:08 +0000

    feat:handling auth

--- src/extension.ts
+++ src/extension.ts
@@ -14,24 +14,24 @@ export async function handleVSCodeCallback(
     const callbackUri = `${vscode.env.uriScheme}://${extensionId}/callback`;
 
     const api = new SpotifyWebApi({
-    clientId:
-      ((await context.secrets.get("clientId")) as string) ||
-      "beb08f57785a4e62822687a9913c6420",
-    clientSecret:
-      ((await context.globalState.get("clientSecret")) as string) ||
-      "73af6bf1e6674c73b36c05a2a660f5f8",
-    redirectUri: callbackUri,
-  });
+      clientId:
+        ((await context.secrets.get("clientId")) as string) ||
+        "beb08f57785a4e62822687a9913c6420",
+      clientSecret:
+        ((await context.globalState.get("clientSecret")) as string) ||
+        "73af6bf1e6674c73b36c05a2a660f5f8",
+      redirectUri: callbackUri,
+    });
 
     // Log the configuration for debugging
     console.log("Authentication Configuration:", {
       callbackUri,
       extensionId: context.extension.id,
-      uriScheme: vscode.env.uriScheme
+      uriScheme: vscode.env.uriScheme,
     });
 
     return api;
-  } catch (error:any) {
+  } catch (error: any) {
     console.error("Failed to configure Spotify API:", error);
     throw new Error(`Failed to configure Spotify API: ${error.message}`);
   }
