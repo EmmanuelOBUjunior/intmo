5ecfc9131dd0b06ae1267269dbc20e0371937459
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 16:16:17 +0000

    feat:working on the playpause command

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 	spotifyApi = new SpotifyWebApi({
 		clientId: context.globalState.get('clientId') as string || 'beb08f57785a4e62822687a9913c6420',
 		clientSecret: context.globalState.get('clientSecret') as string || '73af6bf1e6674c73b36c05a2a660f5f8',
-		redirectUri: context.globalState.get('redirectUri') as string || 'http://127.0.0.1:8888/callback'
+		redirectUri: context.globalState.get('redirectUri') as string || 'http://192.168.0.178:8888/callback'
 	});
 
 	console.log("Spotify API initialized", spotifyApi);
