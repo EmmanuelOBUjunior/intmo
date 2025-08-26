4d55c730b54f08532d36a8ceabf6f566c099ea9d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 16:49:42 +0000

    feat:working on the auth to handle login

--- src/auth.ts
+++ src/auth.ts
@@ -9,5 +9,7 @@ export async function authenticateSpotify(context: vscode.ExtensionContext):Prom
         clientSecret: context.globalState.get('clientSecret') as string || '73af6bf1e6674c73b36c05a2a660f5f8',
         redirectUri: context.globalState.get('redirectUri') as string || 'http://192.168.0.178:8888/callback'
     });
+    const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing'];
+    const authorizeUrl = spotifyApi.createAuthorizeURL(scopes, 'state123');
     return spotifyApi;
 }
