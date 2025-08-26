7eb12ae8eabff6c64508ed300e1b251c3874cf3c
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 16:48:05 +0000

    feat:working on the auth to handle login

--- src/auth.ts
+++ src/auth.ts
@@ -4,5 +4,10 @@ import * as vscode from "vscode";
 
 
 export async function authenticateSpotify(context: vscode.ExtensionContext):Promise<SpotifyWebApi | null>{
-    
+    const spotifyApi = new SpotifyWebApi({
+        clientId: context.globalState.get('clientId') as string || 'beb08f57785a4e62822687a9913c6420',
+        clientSecret: context.globalState.get('clientSecret') as string || '73af6bf1e6674c73b36c05a2a660f5f8',
+        redirectUri: context.globalState.get('redirectUri') as string || 'http://192.168.0.178:8888/callback'
+    });
+    return;
 }
