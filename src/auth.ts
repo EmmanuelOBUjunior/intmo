5402a323f8aa5d8c34a157001bd7e7603f641a6d
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 17:22:09 +0000

    feat:working on the auth to handle login

--- src/auth.ts
+++ src/auth.ts
@@ -7,13 +7,17 @@ import { handleVSCodeCallback } from "./extension";
 
 export async function authenticateSpotify(context: vscode.ExtensionContext):Promise<SpotifyWebApi | null>{
     const api = await handleVSCodeCallback(context);
-    const spotifyApi = new SpotifyWebApi({
-        clientId: context.globalState.get('clientId') as string || 'beb08f57785a4e62822687a9913c6420',
-        clientSecret: context.globalState.get('clientSecret') as string || '73af6bf1e6674c73b36c05a2a660f5f8',
-        redirectUri: context.globalState.get('redirectUri') as string || 'http://192.168.0.178:8888/callback'
-    });
+
+    
+    // const spotifyApi = new SpotifyWebApi({
+    //     clientId: context.globalState.get('clientId') as string || 'beb08f57785a4e62822687a9913c6420',
+    //     clientSecret: context.globalState.get('clientSecret') as string || '73af6bf1e6674c73b36c05a2a660f5f8',
+    //     redirectUri: context.globalState.get('redirectUri') as string || 'http://192.168.0.178:8888/callback'
+    // });
+
+
     const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing'];
-    const authorizeUrl = spotifyApi.createAuthorizeURL(scopes, 'state123');
+    const authorizeUrl = api.createAuthorizeURL(scopes, 'state123');
 
     //Spin up a small local server to catch the callback
     const server = http.createServer(async(req,res)=>{
