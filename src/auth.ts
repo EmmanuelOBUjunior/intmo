c5acb1a0aafdd3b68a6921a5783f42c18b1114ac
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-26 17:24:42 +0000

    feat:working on the auth to handle login

--- src/auth.ts
+++ src/auth.ts
@@ -19,27 +19,32 @@ export async function authenticateSpotify(context: vscode.ExtensionContext):Prom
     const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing'];
     const authorizeUrl = spotifyApi.createAuthorizeURL(scopes, 'state123');
 
-    //Spin up a small local server to catch the callback
-    const server = http.createServer(async(req,res)=>{
-        if(req.url?.startsWith('/callback')){
-            const code = new URL(req.url, 'http://192.168.0.178:8888').searchParams.get("code");
-        
-            if(code){
-                const data = await spotifyApi.authorizationCodeGrant(code);
-                spotifyApi.setAccessToken(data.body.access_token);
-                spotifyApi.setRefreshToken(data.body.access_token);
-
-                //Persist in secrets storage
-                await context.secrets.store('spotifyAccessToken', data.body.access_token);
-                await context.secrets.store('spotifyRefreshToken', data.body.refresh_token);
-
-                res.end("✅ Authentication successful! You can close this tab.");
-                server.close();
-            }
-        }
+    //Handle the callback through VS code's authentication API
+    const result = await vscode.window.showInputBox({
+        prompt: "Please paste the callback URL from your browser after logging in to Spotify"
     });
 
-    server.listen(8888);
+    //Spin up a small local server to catch the callback
+    // const server = http.createServer(async(req,res)=>{
+    //     if(req.url?.startsWith('/callback')){
+    //         const code = new URL(req.url, 'http://192.168.0.178:8888').searchParams.get("code");
+        
+    //         if(code){
+    //             const data = await spotifyApi.authorizationCodeGrant(code);
+    //             spotifyApi.setAccessToken(data.body.access_token);
+    //             spotifyApi.setRefreshToken(data.body.access_token);
+
+    //             //Persist in secrets storage
+    //             await context.secrets.store('spotifyAccessToken', data.body.access_token);
+    //             await context.secrets.store('spotifyRefreshToken', data.body.refresh_token);
+
+    //             res.end("✅ Authentication successful! You can close this tab.");
+    //             server.close();
+    //         }
+    //     }
+    // });
+
+    // server.listen(8888);
     vscode.window.showInformationMessage("🗝️ Logging into Spotify...");
 
     await vscode.env.openExternal(vscode.Uri.parse(authorizeUrl));
