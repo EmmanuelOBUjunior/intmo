f5a04cc468f6b8c9652a3cda2852b8a0feaf9a53
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-28 17:03:34 +0000

    feat: working on the miniplayer class

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,7 +2,10 @@ import * as vscode from 'vscode';
 
 export class MiniplayerPanel{
 
+    public static currentPanel:MiniplayerPanel | undefined;
+    
+
     public static createOrShow(extentionUri: vscode.Uri){
-        
+
     }
 }
