f5532f4d9919e0c64fa3757d48d47c670926be52
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-08-28 17:11:44 +0000

    feat: working on the miniplayer class

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,5 +14,33 @@ export class MiniplayerPanel{
             MiniplayerPanel.currentPanel._panel.reveal(column);
             return;
         }
+
+        //Otherwise, we create one
+        const panel = vscode.window.createWebviewPanel(
+            "spotifyMiniPlayer",
+            "Spotify Mini Player",
+            column,
+            {
+                enableScripts: true,
+                localResourceRoots: [vscode.Uri.joinPath(extentionUri, 'media')]
+            }
+        );
+        MiniplayerPanel.currentPanel = new MiniplayerPanel(panel, extentionUri);
+    }
+
+    private constructor(panel:vscode.WebviewPanel,extensionUri:vscode.Uri){
+        this._panel = panel;
+        this._extensionUri = extensionUri;
+
+        //Initial HTML content
+        this._panel.webview.html = this._getHtmlForWebview(this._panel.webview);
+
+        //Handle disposal
+        this._panel.onDidDispose(()=> this.dispose(), null, []);
+    }
+
+    public dispose(){
+        MiniplayerPanel.currentPanel = undefined;
+        this._panel.dispose();
     }
 }
