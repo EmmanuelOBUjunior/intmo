d00a6acbf1de7d4e2ff1ed01069d8d22201058e2
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-09-05 09:37:25 +0000

    test:writing test for  ensureactivedevices - no devices available

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -9,53 +9,56 @@ suite("Spotify Extension Test Suite", () => {
   let spotifyApi: SpotifyWebApi;
 
   setup(() => {
-        // Create a mock extension context
-        context = {
-            subscriptions: [],
-            workspaceState: {
-                get: () => undefined,
-                update: () => Promise.resolve()
-            },
-            globalState: {
-                get: () => undefined,
-                update: () => Promise.resolve()
-            },
-            secrets: {
-                get: (key: string) => Promise.resolve('dummy-token'),
-                store: (key: string, value: string) => Promise.resolve(),
-                delete: (key: string) => Promise.resolve()
-            },
-            extensionUri: vscode.Uri.file(__dirname),
-            storageUri: vscode.Uri.file(__dirname),
-            globalStorageUri: vscode.Uri.file(__dirname),
-            logUri: vscode.Uri.file(__dirname),
-            extensionMode: vscode.ExtensionMode.Test,
-            extensionPath: __dirname,
-            environmentVariableCollection: {
-                persistent: true,
-                replace: () => {},
-                append: () => {},
-                prepend: () => {},
-                get: () => undefined,
-                forEach: () => {},
-                clear: () => {},
-                delete: () => {},
-                [Symbol.iterator]: function* () { }
-            } as unknown as vscode.EnvironmentVariableCollection,
-            asAbsolutePath: (relativePath: string) => require('path').join(__dirname, relativePath),
-            storagePath: __dirname,
-            globalStoragePath: __dirname,
-            logPath: __dirname
-        } as unknown as vscode.ExtensionContext;
+    // Create a mock extension context
+    context = {
+      subscriptions: [],
+      workspaceState: {
+        get: () => undefined,
+        update: () => Promise.resolve(),
+      },
+      globalState: {
+        get: () => undefined,
+        update: () => Promise.resolve(),
+      },
+      secrets: {
+        get: (key: string) => Promise.resolve("dummy-token"),
+        store: (key: string, value: string) => Promise.resolve(),
+        delete: (key: string) => Promise.resolve(),
+      },
+      extensionUri: vscode.Uri.file(__dirname),
+      storageUri: vscode.Uri.file(__dirname),
+      globalStorageUri: vscode.Uri.file(__dirname),
+      logUri: vscode.Uri.file(__dirname),
+      extensionMode: vscode.ExtensionMode.Test,
+      extensionPath: __dirname,
+      environmentVariableCollection: {
+        persistent: true,
+        replace: () => {},
+        append: () => {},
+        prepend: () => {},
+        get: () => undefined,
+        forEach: () => {},
+        clear: () => {},
+        delete: () => {},
+        [Symbol.iterator]: function* () {},
+      } as unknown as vscode.EnvironmentVariableCollection,
+      asAbsolutePath: (relativePath: string) =>
+        require("path").join(__dirname, relativePath),
+      storagePath: __dirname,
+      globalStoragePath: __dirname,
+      logPath: __dirname,
+    } as unknown as vscode.ExtensionContext;
 
-        // Create a mock Spotify API
-        spotifyApi = new SpotifyWebApi();
-        setSpotifyApi(spotifyApi);
-        setExtensionContext(context);
-    });
+    // Create a mock Spotify API
+    spotifyApi = new SpotifyWebApi();
+    setSpotifyApi(spotifyApi);
+    setExtensionContext(context);
+  });
 
-	test('ensureActiveDevice - no device available',async()=>{
-//Mock getMyDevices to return empty list
-const getDevicesStub = sinon.stub(spotifyApi, 'getMyDevices').resolves({body:{devices:[]}});
-	});
+  test("ensureActiveDevice - no device available", async () => {
+    //Mock getMyDevices to return empty list
+    const getDevicesStub = sinon
+      .stub(spotifyApi, "getMyDevices")
+      .resolves({ body: { devices: [] } });
+  });
 });
