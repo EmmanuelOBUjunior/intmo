1b8821a2639abafa91d1cd07b6d410780e8eb138
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-09-05 09:34:09 +0000

    test:creating a mock spotify api

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -9,31 +9,48 @@ suite("Spotify Extension Test Suite", () => {
   let spotifyApi: SpotifyWebApi;
 
   setup(() => {
-    //Creating a mock extension context
-    context = {
-      subscriptions: [],
-      workspaceState: {
-        get: () => undefined,
-        update: () => Promise.resolve(),
-      },
-      globalState: {
-        get: () => undefined,
-        update: () => Promise.resolve(),
-      },
-      secrets = {
-        get: (key: string) => Promise.resolve("dummy-token"),
-        store: (key: string, value: string) => Promise.resolve(),
-      },
-      extensionUri: vscode.Uri.file(__dirname),
-      storageUri: vscode.Uri.file(__dirname),
-      globalStorageUri: vscode.Uri.file(__dirname),
-      logUri: vscode.Uri.file(__dirname),
-      extensionMode: vscode.ExtensionMode.Test,
-    } as vscode.ExtensionContext;
+        // Create a mock extension context
+        context = {
+            subscriptions: [],
+            workspaceState: {
+                get: () => undefined,
+                update: () => Promise.resolve()
+            },
+            globalState: {
+                get: () => undefined,
+                update: () => Promise.resolve()
+            },
+            secrets: {
+                get: (key: string) => Promise.resolve('dummy-token'),
+                store: (key: string, value: string) => Promise.resolve(),
+                delete: (key: string) => Promise.resolve()
+            },
+            extensionUri: vscode.Uri.file(__dirname),
+            storageUri: vscode.Uri.file(__dirname),
+            globalStorageUri: vscode.Uri.file(__dirname),
+            logUri: vscode.Uri.file(__dirname),
+            extensionMode: vscode.ExtensionMode.Test,
+            extensionPath: __dirname,
+            environmentVariableCollection: {
+                persistent: true,
+                replace: () => {},
+                append: () => {},
+                prepend: () => {},
+                get: () => undefined,
+                forEach: () => {},
+                clear: () => {},
+                delete: () => {},
+                [Symbol.iterator]: function* () { }
+            } as unknown as vscode.EnvironmentVariableCollection,
+            asAbsolutePath: (relativePath: string) => require('path').join(__dirname, relativePath),
+            storagePath: __dirname,
+            globalStoragePath: __dirname,
+            logPath: __dirname
+        } as unknown as vscode.ExtensionContext;
 
-    //Create a mock spotify api
-    spotifyApi = new SpotifyWebApi();
-    setSpotifyApi(spotifyApi);
-    setExtensionContext(context);
-  });
+        // Create a mock Spotify API
+        spotifyApi = new SpotifyWebApi();
+        setSpotifyApi(spotifyApi);
+        setExtensionContext(context);
+    });
 });
