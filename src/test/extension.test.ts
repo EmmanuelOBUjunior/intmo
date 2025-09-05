850f58ef2b063a2401a17c7c7892169fd629e2c7
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-09-05 09:50:19 +0000

    feat:added sinon sandbox

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -15,52 +15,23 @@ suite("Spotify Extension Test Suite", () => {
   let sandBox:sinon.SinonSandbox;
 
   setup(() => {
-	sandBox = sinon.createSandbox();
-    // Create a mock extension context
-    context = {
-      subscriptions: [],
-      workspaceState: {
-        get: () => undefined,
-        update: () => Promise.resolve(),
-      },
-      globalState: {
-        get: () => undefined,
-        update: () => Promise.resolve(),
-      },
-      secrets: {
-        get: (key: string) => Promise.resolve("dummy-token"),
-        store: (key: string, value: string) => Promise.resolve(),
-        delete: (key: string) => Promise.resolve(),
-      },
-      extensionUri: vscode.Uri.file(__dirname),
-      storageUri: vscode.Uri.file(__dirname),
-      globalStorageUri: vscode.Uri.file(__dirname),
-      logUri: vscode.Uri.file(__dirname),
-      extensionMode: vscode.ExtensionMode.Test,
-      extensionPath: __dirname,
-      environmentVariableCollection: {
-        persistent: true,
-        replace: () => {},
-        append: () => {},
-        prepend: () => {},
-        get: () => undefined,
-        forEach: () => {},
-        clear: () => {},
-        delete: () => {},
-        [Symbol.iterator]: function* () {},
-      } as unknown as vscode.EnvironmentVariableCollection,
-      asAbsolutePath: (relativePath: string) =>
-        require("path").join(__dirname, relativePath),
-      storagePath: __dirname,
-      globalStoragePath: __dirname,
-      logPath: __dirname,
-    } as unknown as vscode.ExtensionContext;
+        sandBox = sinon.createSandbox();
+        
+        // Mock extension context
+        context = {
+            subscriptions: [],
+            extensionUri: vscode.Uri.file(__dirname),
+            secrets: {
+                get: sandBox.stub().resolves('dummy-token'),
+                store: sandBox.stub().resolves()
+            }
+        } as unknown as vscode.ExtensionContext;
 
-    // Create a mock Spotify API
-    spotifyApi = new SpotifyWebApi();
-    setSpotifyApi(spotifyApi);
-    setExtensionContext(context);
-  });
+        // Create Spotify API instance
+        spotifyApi = new SpotifyWebApi();
+        setSpotifyApi(spotifyApi);
+        setExtensionContext(context);
+    });
 
   test("ensureActiveDevice - no device available", async () => {
     //Mock getMyDevices to return empty list
