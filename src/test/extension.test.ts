1e0e76fafc262882b7d798fd3458e58e8d969121
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-09-05 09:30:26 +0000

    test:creating a mock spotify api

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,6 +2,7 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 import * as sinon from "sinon";
 import SpotifyWebApi from "spotify-web-api-node";
+import { setExtensionContext, setSpotifyApi } from "../utils/utils";
 
 suite("Spotify Extension Test Suite", () => {
   let context: vscode.ExtensionContext;
@@ -30,4 +31,9 @@ suite("Spotify Extension Test Suite", () => {
       extensionMode: vscode.ExtensionMode.Test,
     } as vscode.ExtensionContext;
   });
+
+  //Create a mock spotify api
+  spotifyApi = new SpotifyWebApi();
+  setSpotifyApi(spotifyApi);
+  setExtensionContext(context);
 });
