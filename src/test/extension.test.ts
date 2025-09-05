08cfc38c19c99a2e2be52e4a6587e9001cb16525
Emmanuel Obu Junior <obu.junior.emmanuel@gmail.com>
2025-09-05 09:29:31 +0000

    test:creating a mock extension context

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,1 +1,33 @@
-import * as assert from 'assert';
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as sinon from "sinon";
+import SpotifyWebApi from "spotify-web-api-node";
+
+suite("Spotify Extension Test Suite", () => {
+  let context: vscode.ExtensionContext;
+  let spotifyApi: SpotifyWebApi;
+
+  setup(() => {
+    //Creating a mock extension context
+    context = {
+      subscriptions: [],
+      workspaceState: {
+        get: () => undefined,
+        update: () => Promise.resolve(),
+      },
+      globalState: {
+        get: () => undefined,
+        update: () => Promise.resolve(),
+      },
+      secrets = {
+        get: (key: string) => Promise.resolve("dummy-token"),
+        store: (key: string, value: string) => Promise.resolve(),
+      },
+      extensionUri: vscode.Uri.file(__dirname),
+      storageUri: vscode.Uri.file(__dirname),
+      globalStorageUri: vscode.Uri.file(__dirname),
+      logUri: vscode.Uri.file(__dirname),
+      extensionMode: vscode.ExtensionMode.Test,
+    } as vscode.ExtensionContext;
+  });
+});
